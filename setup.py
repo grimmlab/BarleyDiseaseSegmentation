32bdef3ec2e1eb1ccb5b5069ab646779de612205
Sofia Martello <118368494+sofiamrtll@users.noreply.github.com>
2026-02-04 09:38:18 +0100

    # package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="barley_disease_segmentation",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Core utilities and configuration for BarleyDiseaseSegmentation project",
+)
